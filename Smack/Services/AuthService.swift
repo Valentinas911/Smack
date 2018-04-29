b769ba60b22fce4caacfe12486739f3bbc373fd7
Valentinas Mirosnicenko <valentinasmirosnicenko@Valentinass-MacBook-Pro-5.local>
2018-04-29 16:04:46 +0800

    Lesson 11

--- Smack/Services/AuthService.swift
+++ Smack/Services/AuthService.swift
@@ -8,6 +8,7 @@
 
 import Foundation
 import Alamofire
+import SwiftyJSON
 
 
 class AuthService {
@@ -42,18 +43,14 @@ class AuthService {
     
     func registerUser(email: String, password: String, completion: @escaping CompletionHandler) {
         
-        let lowerCaseEmal = email.lowercased()
+        let lowerCasedEmail = email.lowercased()
 
-        let header = [
-            "Content-Type":"application/json; charset=utf-8"
-        ]
-        
         let body:[String:Any] = [
-            "email":lowerCaseEmal,
+            "email":lowerCasedEmail,
             "password":password
         ]
         
-        Alamofire.request(URL_REGISTER, method: .post, parameters: body, encoding: JSONEncoding.default, headers: header).responseString { (response) in
+        Alamofire.request(URL_REGISTER, method: .post, parameters: body, encoding: JSONEncoding.default, headers: HEADER).responseString { (response) in
             if response.result.error == nil {
                 completion(true)
                 print(response.result)
@@ -61,6 +58,62 @@ class AuthService {
                 completion(false)
                 debugPrint(response.result.error.debugDescription)
             }
+        }
+        
+    }
+    
+    func loginUser(email: String, password: String, completion: @escaping CompletionHandler) {
+        let lowerCasedEmail = email.lowercased()
+        
+        let body:[String:Any] = [
+            "email":lowerCasedEmail,
+            "password":password
+        ]
+        
+        Alamofire.request(URL_LOGIN, method: .post, parameters: body, encoding: JSONEncoding.default, headers: HEADER).responseJSON { (response) in
+            if response.result.error == nil {
+                
+                // Method 1 (no SwiftyJSON)
+//                if let json = response.result.value as? Dictionary<String,Any> {
+//                    if let email = json["user"] as? String {
+//                        self.userEmail = email
+//                    }
+//
+//                    if let token = json["token"] as? String {
+//                        self.authToken = token
+//                    }
+//                }
+                
+                // This is only used with Method 2 and 3
+//                guard let data = response.data else { return }
+                
+                // Method 2 (Using SwiftyJSON)
+//                do {
+//                    let json = try JSON(data: data)
+//                    self.userEmail = json["user"].stringValue
+//                    self.authToken = json["token"].stringValue
+//                } catch {
+//                    debugPrint("Error when converting to JSON Object")
+//                }
+                
+                // Method 3 (Using SwiftyJSON) + if let
+//                if let json = try? JSON(data:data) {
+//                    self.userEmail = json["user"].stringValue
+//                    self.authToken = json["token"].stringValue
+//                }
+                
+                // Method 4 (Using SwiftyJSON) + guard let
+                guard let data = response.data, let json = try? JSON(data:data) else { return }
+                self.userEmail = json["user"].stringValue
+                self.authToken = json["token"].stringValue
+                
+                self.isLoggedIn = true
+                completion(true)
+            } else {
+                completion(false)
+                debugPrint(response.result.error.debugDescription)
+            }
+            
         }
         
     }
