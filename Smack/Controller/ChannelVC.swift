8ebbb59bd1eb45dafb26847fe8de11570d2bd1f5
Valentinas Mirosnicenko <valentinasmirosnicenko@Valentinass-MacBook-Pro-5.local>
2018-04-30 13:18:35 +0800

    Lesson 21

--- Smack/Controller/ChannelVC.swift
+++ Smack/Controller/ChannelVC.swift
@@ -13,9 +13,13 @@ class ChannelVC: UIViewController {
     
     @IBOutlet weak var loginButton: UIButton!
     @IBOutlet weak var userImage: CircleImage!
+    @IBOutlet weak var tableView: UITableView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        tableView.delegate = self
+        tableView.dataSource = self
 
         self.revealViewController().rearViewRevealWidth = self.view.frame.size.width - 60
         
@@ -61,5 +65,31 @@ class ChannelVC: UIViewController {
         }
     }
     
+
+}
+
+extension ChannelVC: UITableViewDelegate, UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        if let cell = tableView.dequeueReusableCell(withIdentifier: "channelCell", for: indexPath) as? ChannelCell {
+            let channel = MessageService.instance.channels[indexPath.row]
+            cell.configureCell(channel: channel)
+            return cell
+        } else {
+            return ChannelCell()
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return MessageService.instance.channels.count
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
 
 }
---
+++ Smack/View/ChannelCell.swift
@@ -0,0 +1,38 @@
+//
+//  ChannelCell.swift
+//  Smack
+//
+//  Created by Valentinas Mirosnicenko on 4/30/18.
+//  Copyright © 2018 Valentinas Mirosnicenko. All rights reserved.
+//
+
+import UIKit
+
+class ChannelCell: UITableViewCell {
+
+    // Outlets
+    @IBOutlet weak var channelName: UILabel!
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    
+    
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+        
+        if selected {
+            self.layer.backgroundColor = UIColor(white: 1.0, alpha: 0.2).cgColor
+        } else {
+            self.layer.backgroundColor = UIColor.clear.cgColor
+        }
+        
+    }
+    
+    func configureCell(channel: Channel) {
+        channelName.text = "#\(channel.name ?? "N/A")"
+    }
+
+}
